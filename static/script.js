685f528216e6a9f18da313e158b9a8c9f6329fdb
Tom Elliott <tmelliottjr@gmail.com>
2017-12-01 16:46:06 -0500

    front end updates to display network status

---
+++ static/script.js
@@ -0,0 +1,37 @@
+var networkTableHead = "<tr><th>Network Name</th><th>Status</th></tr>"
+var updateInterval = 30 // In seconds
+
+function fetch_network_status(){
+    $('#last_updated').html("Last Updated: Updating now...")
+    $.ajax({
+        url: '/fetch_all'
+    }).done(network_status_handler)
+
+}
+
+function network_status_handler(network_statuses){
+    console.log(network_statuses)
+    var networkTable = $("#network_status")
+    networkTable.html(networkTableHead)
+
+    network_statuses.forEach(function(network){
+        networkTable.html(networkTable.html()+'<tr><th>'+network.name+'</th><th>'+network.status+'</th></tr>')
+    })
+
+     $('#last_updated').html("Last Updated: "+formatDate(new Date))
+}
+
+function formatDate(date) {
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+  var ampm = hours >= 12 ? 'pm' : 'am';
+  hours = hours % 12;
+  hours = hours ? hours : 12;
+  minutes = minutes < 10 ? '0'+minutes : minutes;
+  var strTime = hours + ':' + minutes + ' ' + ampm;
+  return date.getMonth()+1 + "/" + date.getDate() + "/" + date.getFullYear() + " " + strTime;
+}
+
+fetch_network_status()
+
+setInterval(fetch_network_status, updateInterval * 1000)
