a98f983c4cf7231dfc6f83d27fad7306d3933fc2
John Henise <john.henise@gmail.com>
2017-12-01 14:38:34 -0500

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,40 @@
+from meraki import meraki
+
+#Pull API Key from file
+file = open("api_key.txt", "r")
+api_key = file.read()
+#api_key = [insert here]
+#print(api_key)
+
+#pull Org ID from file
+file = open("org_id.txt", "r")
+org_id = file.read()
+#org_id = '316511'
+#print(org_id)
+
+#Retrieve list of networks in organization
+networks = meraki.getnetworklist(api_key, org_id, suppressprint=True)
+
+#Create output file named output.txt
+output_file = open("output.txt", "w")
+
+#iterate through each device in each network, check status of device/uplink and set variable 'network_status' to "Down" if applicable
+#if device/uplink is down, skip rest of loop, print findings, and continue with next network
+for network in networks :
+    network_status = "Up";
+    devices = meraki.getnetworkdevices(api_key, network['id'], suppressprint=True)
+    for device in devices:
+        if network_status == "Down" : break
+        uplinks = meraki.getdeviceuplink(api_key, network['id'], device['serial'],suppressprint=True)
+        for uplink in uplinks :
+            if (uplink['status'] == "Failed") :
+                network_status = "Down"
+                break
+
+    #print status of each network to console and output.txt
+    print(network['name'] + " - " + network_status)
+    output_file.write(network['name'] + " - " + network_status + "\n")
+
+output_file.close()
+
+print("Executed successfully...")
---
+++ main.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request, jsonify, url_for
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/meraki")
+def meraki():
+    return render_template("meraki.html")
+
+@app.route("/n9k")
+def n9k():
+    return render_template("nexus9k.html")
+
+@app.route("/forward/", methods=['POST'])
+def move_forward():
+    #Moving forward code
+    print("Hey Cliff")
+    forward_message = "Moving Forward..."
+    return render_template('index.html', message=forward_message);
+
+#@app.route("/functions")
+#def functions():
+#    return  "some json here"
+
+if __name__ == "__main__":
+    app.run(debug=True)
