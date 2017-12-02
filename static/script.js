86841659828525757e3a09a0b284d0dfa5c34d06
Tom Elliott <tmelliottjr@gmail.com>
2017-12-01 20:25:19 -0500

    modularize code

--- static/script.js
+++ static/script.js
@@ -1,44 +1,52 @@
-var networkTableHead = "<tr><th>Network Name</th><th>Status</th></tr>"
-var updateInterval = 30 // In seconds
-
-function fetch_network_status(){
-    $('#last_updated').html("Last Updated: Updating now...")
-    $.ajax({
-        url: '/fetch_all'
-    }).done(network_status_handler)
-
-}
-
-function network_status_handler(network_statuses){
-    console.log(network_statuses)
-    var networkTable = $("#network_status")
-    networkTable.html(networkTableHead)
-
-
-    network_statuses.forEach(function(network){
-        var status = 'status_'
-        if (network.status == "Up"){
-             status += 'up'
-        } else {
-             status +='down'
-        }
-        networkTable.html(networkTable.html()+'<tr><th>'+network.name+'</th><th><div class="'+status+'"></div>'+network.status+'</th></tr>')
-    })
-
-     $('#last_updated').html("Last Updated: "+formatDate(new Date))
-}
-
 function formatDate(date) {
-  var hours = date.getHours();
-  var minutes = date.getMinutes();
-  var ampm = hours >= 12 ? 'pm' : 'am';
-  hours = hours % 12;
-  hours = hours ? hours : 12;
-  minutes = minutes < 10 ? '0'+minutes : minutes;
-  var strTime = hours + ':' + minutes + ' ' + ampm;
-  return date.getMonth()+1 + "/" + date.getDate() + "/" + date.getFullYear() + " " + strTime;
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  const ampm = hours >= 12 ? 'pm' : 'am';
+  hours %= 12;
+  hours = hours || 12;
+  minutes = minutes < 10 ? `0${minutes}` : minutes;
+  const strTime = `${hours}:${minutes} ${ampm}`;
+  return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()} ${strTime}`;
 }
 
-fetch_network_status()
+const NetworkStatus = {
+  settings: {
+    updateInterval: 30,
+    networkTableHead: '<tr><th>Network Name</th><th>Status</th></tr>',
+  },
+  el: {
+    lastUpdatedDisplay: $('#last_updated'),
+    networkTable: $('#network_status'),
+  },
+  init() {
+    this.fetch_network_status();
+    this.startTimer();
+  },
+  startTimer() {
+    setInterval(this.fetch_network_status, this.settings.updateInterval * 1000);
+  },
+  fetch_network_status() {
+    this.el.lastUpdatedDisplay.html('Last Updated: Updating now...');
+    $.ajax({
+      url: '/fetch_all',
+    }).done(this.network_status_handler.bind(this));
+  },
+  network_status_handler(result) {
+    this.el.networkTable.html(this.settings.networkTableHead);
+    result.forEach(this.tableAppender.bind(this));
 
-setInterval(fetch_network_status, updateInterval * 1000)
+    this.el.lastUpdatedDisplay.html(`Last Updated: ${formatDate(new Date())}`);
+  },
+  tableAppender(network) {
+    let status = 'status_';
+    if (network.status === 'Up') {
+      status += 'up';
+    } else {
+      status += 'down';
+    }
+    this.el.networkTable.html(`${this.el.networkTable.html()}<tr><th>${network.name}</th><th><div class="${status}"></div>${network.status}</th></tr>`);
+  },
+
+};
+
+NetworkStatus.init();
